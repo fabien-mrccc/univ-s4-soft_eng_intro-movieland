f0f9ddd9eee7ce22618bb54198e9d19d9ef9e78f
Fabien-mrccc <fabienmarcuccini.pro@gmail.com>
2024-01-24 11:31:37 +0100

    add void testContains()

--- app/src/test/java/moviesapp/model/FavoritesTest.java
+++ app/src/test/java/moviesapp/model/FavoritesTest.java
@@ -1,7 +1,25 @@
 package moviesapp.model;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FavoritesTest {
+
+    @Test
+    void testAdd(){
+        Favorites favorites = new Favorites();
+        Movie movie1 = new Movie();
+
+    }
+
+    @Test
+    void testContains(){
+        Favorites favorites = new Favorites();
+        Movie movie = new Movie();
+
+        assertThat(favorites.contains(movie)).isEqualTo(false);
+        favorites.add(movie);
+        assertThat(favorites.contains(movie)).isEqualTo(true);
+    }
 }
