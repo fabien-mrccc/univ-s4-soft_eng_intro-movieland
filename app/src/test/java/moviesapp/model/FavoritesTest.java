26b03fa8ca096304c752f73f619fc1afd1e6288d
Fabien-mrccc <fabienmarcuccini.pro@gmail.com>
2024-01-24 12:08:19 +0100

    fix void testContains(): add a favorites attribute and delete the local variable from the method

--- app/src/test/java/moviesapp/model/FavoritesTest.java
+++ app/src/test/java/moviesapp/model/FavoritesTest.java
@@ -1,25 +1,29 @@
 package moviesapp.model;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FavoritesTest {
+    private Favorites favorites;
 
     @Test
-    void testAdd(){
+    void testAdd() {
         Favorites favorites = new Favorites();
         Movie movie1 = new Movie();
+        Movie movie2 = new Movie();
 
+        assertThat(favorites.contains(movie1)).isEqualTo(false);
+        favorites.add(movie1);
+        assertThat(favorites.contains(movie1)).isEqualTo(true);
     }
 
     @Test
-    void testContains(){
-        Favorites favorites = new Favorites();
+    void testContains() {
         Movie movie = new Movie();
 
         assertThat(favorites.contains(movie)).isEqualTo(false);
         favorites.add(movie);
         assertThat(favorites.contains(movie)).isEqualTo(true);
+        assertThat(favorites.contains(null)).isEqualTo(false);
     }
 }
