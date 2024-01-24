2f7919a5b8fdd9bfa5b891237ae2c9e2adafffc2
Fabien-mrccc <fabienmarcuccini.pro@gmail.com>
2024-01-24 11:21:28 +0100

    add public boolean contains(Movie movie)

--- app/src/main/java/moviesapp/model/Favorites.java
+++ app/src/main/java/moviesapp/model/Favorites.java
@@ -9,6 +9,7 @@ public class Favorites {
     public boolean isEmpty(){
         return favorites.isEmpty();
     }
+
     /** Add a film to the favorites of the user
      @param movie
      **/
@@ -16,5 +17,14 @@ public class Favorites {
         if(!favorites.contains(movie)){
             favorites.add(movie);
         }
+    }
+
+    /**
+     * Check if the favorites list contains a specific movie
+     * @param movie the movie that we check if it is in our list
+     * @return boolean
+     */
+    public boolean contains(Movie movie){
+        return favorites.contains(movie);
     }
 }
