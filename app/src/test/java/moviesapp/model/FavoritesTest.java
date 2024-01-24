0551cd07e2e069a2cfcd716f628be61c75c15e32
fanani <amina.fanani@etu.univ-amu.fr>
2024-01-24 12:29:50 +0100

    add void testRemove()

--- app/src/test/java/moviesapp/model/FavoritesTest.java
+++ app/src/test/java/moviesapp/model/FavoritesTest.java
@@ -1,8 +1,8 @@
 package moviesapp.model;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeAll;
 
 public class FavoritesTest {
     private static Favorites favorites;
@@ -17,7 +17,17 @@ public class FavoritesTest {
         Movie movie1 = new Movie();
         Movie movie2 = new Movie();
     }
-
+    @Test
+    void testRemove(){
+        Movie movie1 = new Movie();
+        Movie movie2 = new Movie();
+        favorites.add(movie1);
+        favorites.add(movie2);
+        favorites.remove(movie1);
+        assertThat(favorites.toString()).isEqualTo(movie2.toString());
+        favorites.remove(movie2);
+        assertThat(favorites.toString()).isEqualTo("Your list of favorites is empty.");
+    }
     @Test
     void testContains() {
         Movie movie = new Movie();
