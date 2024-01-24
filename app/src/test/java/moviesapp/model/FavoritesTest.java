f6b14d35408a45dd6e4ba346ef605d7961cd369d
Fabien-mrccc <fabienmarcuccini.pro@gmail.com>
2024-01-24 12:49:57 +0100

    add void testToString(){

--- app/src/test/java/moviesapp/model/FavoritesTest.java
+++ app/src/test/java/moviesapp/model/FavoritesTest.java
@@ -6,6 +6,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FavoritesTest {
     private static Favorites favorites;
+    private final Movie movie1 = new Movie();
+    private final Movie movie2 = new Movie();
+    private final Movie movie3 = new Movie();
 
     @BeforeAll
     static void setupBeforeAll(){
@@ -14,7 +17,31 @@ public class FavoritesTest {
 
     @Test
     void testAdd() {
-        Movie movie1 = new Movie();
-        Movie movie2 = new Movie();
+    }
+
+    @Test
+    void testToString(){
+        if(!favorites.isEmpty()){
+            favorites.clear();
+        }
+        assertThat(favorites.toString()).isEqualTo("Your list of favorites is empty.");
+        favorites.add(movie1);
+        assertThat(favorites.toString()).isEqualTo(movie1.toString());
+        favorites.add(movie2);
+        favorites.add(movie3);
+        assertThat(favorites.toString()).isEqualTo(
+                movie1
+                        + "\n"
+                        + movie2
+                        + "\n"
+                        + movie3);
+        favorites.remove(movie2);
+        assertThat(favorites.toString()).isEqualTo(
+                movie1
+                        + "\n"
+                        + movie3);
+        favorites.remove(movie1);
+        favorites.remove(movie3);
+        assertThat(favorites.toString()).isEqualTo("Your list of favorites is empty.");
     }
 }
