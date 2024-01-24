3e4820d4345d1e4f106651cdf8a37f54f2e1fa22
fanani <amina.fanani@etu.univ-amu.fr>
2024-01-24 11:21:00 +0100

    add public void removeFromFavorites(Movie movie)

--- app/src/main/java/moviesapp/model/Favorites.java
+++ app/src/main/java/moviesapp/model/Favorites.java
@@ -9,12 +9,23 @@ public class Favorites {
     public boolean isEmpty(){
         return favorites.isEmpty();
     }
+
     /** Add a film to the favorites of the user
-     @param movie
+     @param movie the movie to add to the list
      **/
     public void addToFavorites(Movie movie){
         if(!favorites.contains(movie)){
             favorites.add(movie);
+        }
+    }
+
+    /** Remove a film from the list of favorites
+     @param movie the movie to remove from the list
+     **/
+    public void removeFromFavorites(Movie movie){
+        try {favorites.remove(movie);}
+        catch (UnsupportedOperationException e){
+            System.out.println("This movie does not belong to your list of favorites");
         }
     }
 }
