71ca98e62f4eb72cc56a431713576803170b8f69
numa <numa.efremon@etu.univ-amu.fr>
2024-01-25 01:40:03 +0100

    completion of the function so that it takes all the information from the films and then displays it

--- app/src/main/java/moviesapp/model/reader.java
+++ app/src/main/java/moviesapp/model/reader.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 
 public class reader {
-    public void Print_name(){
+    public void Print_information(){
         try{
             String filePath = "s_team_pigl_moviesapp/app/src/main/java/moviesapp/model/données.json";
             ObjectMapper objectMapper = new ObjectMapper();
@@ -14,8 +14,34 @@ public class reader {
             JsonNode resultsNode = jsonNode.get("results");
 
             for(JsonNode result : resultsNode){
-                String Name = result.get("original_title").asText() ;
-                System.out.println("Title: " + Name);
+                boolean adult = result.get("adult").asBoolean() ;
+                String backdrop_path = result.get("backdrop_path").asText() ;
+                int id = result.get("id").asInt() ;
+                String language = result.get("original_language").asText() ;
+                String original_title = result.get("original_title").asText() ;
+                String overview = result.get("overview").asText() ;
+                double popularity = result.get("popularity").asDouble() ;
+                String poster_path = result.get("poster_path").asText() ;
+                String release = result.get("release_date").asText() ;
+                String title = result.get("title").asText() ;
+                boolean video = result.get("video").asBoolean() ;
+                double vote_average = result.get("vote_average").asDouble() ;
+                double vote_count  =result.get("vote_count").asDouble() ;
+                System.out.println("--------------------") ;
+                System.out.println("adulte : " + adult);
+                System.out.println("backdrop_path : " + backdrop_path);
+                System.out.println("id : " + id);
+                System.out.println("language : " + language);
+                System.out.println("original_title : " + original_title);
+                System.out.println("overview : " + overview);
+                System.out.println("popularity : " + popularity);
+                System.out.println("poster_path : " + poster_path);
+                System.out.println("release : " + release);
+                System.out.println("title : " + title);
+                System.out.println("video : " + video);
+                System.out.println("vote_average : " + vote_average);
+                System.out.println("vote_count : " + vote_count);
+                System.out.println("--------------------") ;
             }
 
         } catch (Exception e) {
