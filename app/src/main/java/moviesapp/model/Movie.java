3e8017db4b796a3797673fb8d46cd16f54ad13a3
Fabien MARCUCCINI <fabien.marcuccini@etu.univ-amu.fr>
2024-01-27 16:33:31 +0100

    fix overview attribute name (was with an UpperCase)

--- app/src/main/java/moviesapp/model/Movie.java
+++ app/src/main/java/moviesapp/model/Movie.java
@@ -1,6 +1,6 @@
 package moviesapp.model;
 
 public record Movie(boolean adult, String backdropPath, String genreIds, int id, String originalLanguage,
-                    String originalTittle, String Overview, double popularity, String posterPath, String releaseDate,
+                    String originalTittle, String overview, double popularity, String posterPath, String releaseDate,
                     String title, boolean video, double voteAverage, int voteCount) {
 }
