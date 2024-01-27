6e7510112ae2f3a82df2151478372c0e11b0c207
Fabien MARCUCCINI <fabien.marcuccini@etu.univ-amu.fr>
2024-01-27 16:24:22 +0100

    fix public Movie findMovie(int movieID)

--- app/src/main/java/moviesapp/model/JSONReader.java
+++ app/src/main/java/moviesapp/model/JSONReader.java
@@ -1,8 +1,6 @@
 package moviesapp.model;
 import com.fasterxml.jackson.databind.*;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 public class JSONReader {
     private final File jsonFile;
@@ -19,45 +17,33 @@ public class JSONReader {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode jsonNode = objectMapper.readTree(jsonFile);
-            int page = jsonNode.get("page").asInt();
-            JsonNode resultsNode = jsonNode.get("results");
-            List<JsonNode> elt = new ArrayList<>();
+            JsonNode movieSelected = null;
 
-            for(JsonNode result : resultsNode ){
-                if(result.get("id").asInt() == movieID){
-                    elt.add(result) ;
+            JsonNode jsonMovies = jsonNode.get("results");
+
+            for(JsonNode jsonMovie : jsonMovies ){
+                if(jsonMovie.get("id").asInt() == movieID){
+                    movieSelected = jsonMovie;
                 }
             }
 
-            for (JsonNode result : elt) {
-                boolean adult = result.get("adult").asBoolean() ;
-                String backdrop_path = result.get("backdrop_path").asText() ;
-                int Id = result.get("id").asInt() ;
-                String language = result.get("original_language").asText() ;
-                String original_title = result.get("original_title").asText() ;
-                String overview = result.get("overview").asText() ;
-                double popularity = result.get("popularity").asDouble() ;
-                String poster_path = result.get("poster_path").asText() ;
-                String release = result.get("release_date").asText() ;
-                String title = result.get("title").asText() ;
-                boolean video = result.get("video").asBoolean() ;
-                double vote_average = result.get("vote_average").asDouble() ;
-                double vote_count  =result.get("vote_count").asDouble() ;
-                System.out.println("--------------------") ;
-                System.out.println("adulte : " + adult);
-                System.out.println("backdrop_path : " + backdrop_path);
-                System.out.println("movieID : " + movieID);
-                System.out.println("language : " + language);
-                System.out.println("original_title : " + original_title);
-                System.out.println("overview : " + overview);
-                System.out.println("popularity : " + popularity);
-                System.out.println("poster_path : " + poster_path);
-                System.out.println("release : " + release);
-                System.out.println("title : " + title);
-                System.out.println("video : " + video);
-                System.out.println("vote_average : " + vote_average);
-                System.out.println("vote_count : " + vote_count);
-                System.out.println("--------------------") ;
+            if(movieSelected != null){
+                return new Movie(
+                        movieSelected.get("adult").asBoolean(),
+                        movieSelected.get("backdrop_path").asText(),
+                        movieSelected.get("genre_ids").asText(),
+                        movieSelected.get("id").asInt(),
+                        movieSelected.get("original_language").asText(),
+                        movieSelected.get("original_title").asText(),
+                        movieSelected.get("overview").asText(),
+                        movieSelected.get("popularity").asDouble(),
+                        movieSelected.get("poster_path").asText(),
+                        movieSelected.get("release_date").asText(),
+                        movieSelected.get("title").asText(),
+                        movieSelected.get("video").asBoolean(),
+                        movieSelected.get("vote_average").asDouble(),
+                        movieSelected.get("vote_count").asInt()
+                );
             }
         }
         catch (Exception e) {
