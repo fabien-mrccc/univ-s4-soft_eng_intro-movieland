2dc94917781ff4dbedd78a4672c4b890791c4315
Fabien MARCUCCINI <fabien.marcuccini@etu.univ-amu.fr>
2024-01-27 17:07:38 +0100

    fix genreIds attribute type from String to List<Integer>

--- app/src/main/java/moviesapp/model/Movie.java
+++ app/src/main/java/moviesapp/model/Movie.java
@@ -1,6 +1,8 @@
 package moviesapp.model;
 
-public record Movie(boolean adult, String backdropPath, String genreIds, int id, String originalLanguage,
+import java.util.List;
+
+public record Movie(boolean adult, String backdropPath, List<Integer> genreIds, int id, String originalLanguage,
                     String originalTittle, String overview, double popularity, String posterPath, String releaseDate,
                     String title, boolean video, double voteAverage, int voteCount) {
 }
