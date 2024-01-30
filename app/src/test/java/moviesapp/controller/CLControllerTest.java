d6e52530fc81ee16e56c7b4629c24879c3501a64
m22027323 <fabien.marcuccini@etu.univ-amu.fr>
2024-01-30 17:15:51 +0100

    add private void provideInput(String data)

--- app/src/test/java/moviesapp/controller/CLControllerTest.java
+++ app/src/test/java/moviesapp/controller/CLControllerTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 
 import moviesapp.model.Favorites;
 
+import java.io.ByteArrayInputStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CLControllerTest {
@@ -17,18 +19,29 @@ public class CLControllerTest {
     static void setupBeforeAll(){
         jsonReader = new JSONReader(System.getProperty("user.dir")+"/src/test/java/moviesapp/model/data_example.json");
     }
+
+    private void provideInput(String data) {
+        ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    /*
+
     @Test
     public void clearTest(){
-        CLController controlle = new CLController() ;
+        CLController controller = new CLController() ;
         Movie movie = jsonReader.findMovie(32571) ;
         Movie movie2 = jsonReader.findMovie(32571) ;
         Favorites.instance.add(movie);
         Favorites.instance.add(movie2);
-        controlle.clear("yes");
+        controller.clear("yes");
         assertThat(Favorites.instance.isEmpty()).isTrue();
+    }
 
+     */
 
-
+    @Test
+    void testExit(){
 
     }
 
