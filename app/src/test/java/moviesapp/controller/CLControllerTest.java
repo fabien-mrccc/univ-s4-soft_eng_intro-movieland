eab706bd8c2c15b089f8c5ff510c8fd1005e5923
m22027323 <fabien.marcuccini@etu.univ-amu.fr>
2024-01-30 17:31:54 +0100

    remove unused tests in CLControllerTest.java

--- app/src/test/java/moviesapp/controller/CLControllerTest.java
+++ app/src/test/java/moviesapp/controller/CLControllerTest.java
@@ -1,48 +1,15 @@
 package moviesapp.controller;
-import moviesapp.model.Movie;
 import moviesapp.model.JSONReader;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-
-
-import moviesapp.model.Favorites;
-
-import java.io.ByteArrayInputStream;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class CLControllerTest {
     private static JSONReader jsonReader;
 
+    private static CLController controller;
+
     @BeforeAll
     static void setupBeforeAll(){
         jsonReader = new JSONReader(System.getProperty("user.dir")+"/src/test/java/moviesapp/model/data_example.json");
+        controller = new CLController();
     }
-
-    private void provideInput(String data) {
-        ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
-        System.setIn(testIn);
-    }
-
-    /*
-
-    @Test
-    public void clearTest(){
-        CLController controller = new CLController() ;
-        Movie movie = jsonReader.findMovie(32571) ;
-        Movie movie2 = jsonReader.findMovie(32571) ;
-        Favorites.instance.add(movie);
-        Favorites.instance.add(movie2);
-        controller.clear("yes");
-        assertThat(Favorites.instance.isEmpty()).isTrue();
-    }
-
-     */
-
-    @Test
-    void testExit(){
-
-    }
-
 }
