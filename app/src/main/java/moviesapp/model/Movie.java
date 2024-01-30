3bb26eabb4a14db5f88293d9607639f33f874a06
Amina <amina.fanani@etu.univ-amu.fr>
2024-01-30 15:12:19 +0100

    add public String toString()

--- app/src/main/java/moviesapp/model/Movie.java
+++ app/src/main/java/moviesapp/model/Movie.java
@@ -3,6 +3,14 @@ package moviesapp.model;
 import java.util.List;
 
 public record Movie(boolean adult, String backdropPath, List<Integer> genreIds, int id, String originalLanguage,
-                    String originalTittle, String overview, double popularity, String posterPath, String releaseDate,
+                    String originalTitle, String overview, double popularity, String posterPath, String releaseDate,
                     String title, boolean video, double voteAverage, int voteCount) {
+    @Override
+    public String toString(){
+        return "movie n°"+ id +":\n  title: "+title+"\n  original title: "+originalTitle+
+                "\n  release date: "+releaseDate+ "\n  original language: "+originalLanguage+
+                "\n  genreIds: "+genreIds+"\n  popularity: " +popularity+"\n  adult: "+adult+
+                "\n  vote average: "+voteAverage+"\n vote count: " +voteCount+"\n  overview:\n"
+                +overview;
+    }
 }
