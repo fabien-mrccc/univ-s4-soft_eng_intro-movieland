e0fe3a696119b5e9d1d7b2480a80e5fe2c2cf9d2
Fabien MARCUCCINI <fabien.marcuccini@etu.univ-amu.fr>
2024-02-03 20:24:29 +0100

    add \n after welcome message

--- app/src/main/java/moviesapp/AppCLI.java
+++ app/src/main/java/moviesapp/AppCLI.java
@@ -5,7 +5,7 @@ import moviesapp.controller.CLController;
 
 public class AppCLI {
     public static void main(String[] args) {
-        System.out.println("\nWelcome to the movies app!");
+        System.out.println("\nWelcome to the movies app!\n");
         CLController controller = new CLController();
         controller.select();
     }
