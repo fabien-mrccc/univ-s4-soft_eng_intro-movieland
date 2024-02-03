9e06c87dd14e70fcb9d44ee94c2290133d431d88
aminafanani <amina.fanani@etu.univ-amu.fr>
2024-02-03 15:06:23 +0100

    fix public String toString()

--- app/src/main/java/moviesapp/model/Movie.java
+++ app/src/main/java/moviesapp/model/Movie.java
@@ -7,10 +7,10 @@ public record Movie(boolean adult, String backdropPath, List<Integer> genreIds, 
                     String title, boolean video, double voteAverage, int voteCount) {
     @Override
     public String toString(){
-        return "movie n°"+ id +":\n  title: "+title+"\n  original title: "+originalTitle+
+        return "__________________________  \n"+"movie n°"+ id +":\n  title: "+title+"\n  original title: "+originalTitle+
                 "\n  release date: "+releaseDate+ "\n  original language: "+originalLanguage+
                 "\n  genreIds: "+genreIds+"\n  popularity: " +popularity+"\n  adult: "+adult+
                 "\n  vote average: "+voteAverage+"\n  vote count: " +voteCount+"\n  overview: "
-                +overview;
+                +overview+ "\n__________________________  ";
     }
 }
