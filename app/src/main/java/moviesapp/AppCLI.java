80eb60fdbdfe89cb396c0183d16af469dbde8d99
Fabien MARCUCCINI <fabien.marcuccini@etu.univ-amu.fr>
2024-02-03 20:48:23 +0100

    add  System.out.println("Type 'help' to learn application commands.");

--- app/src/main/java/moviesapp/AppCLI.java
+++ app/src/main/java/moviesapp/AppCLI.java
@@ -6,6 +6,7 @@ import moviesapp.controller.CLController;
 public class AppCLI {
     public static void main(String[] args) {
         System.out.println("\nWelcome to the movies app!");
+        System.out.println("Type 'help' to learn application commands.");
         CLController controller = new CLController();
         controller.select();
     }
