0384c0ac09fe0e4f1e0ae8b7598cffcb0ff4c495
Fabien MARCUCCINI <fabien.marcuccini@etu.univ-amu.fr>
2024-02-03 20:12:25 +0100

    remove unused prompt to user

--- app/src/main/java/moviesapp/AppCLI.java
+++ app/src/main/java/moviesapp/AppCLI.java
@@ -5,10 +5,8 @@ import moviesapp.controller.CLController;
 
 public class AppCLI {
     public static void main(String[] args) {
-        System.out.println("Welcome to the movies app");
+        System.out.println("\nWelcome to the movies app!");
         CLController controller = new CLController();
-
-        System.out.println("You requested command '" + args[0] + "' with parameter '" + args[1] + "'");
         controller.select();
     }
 }
