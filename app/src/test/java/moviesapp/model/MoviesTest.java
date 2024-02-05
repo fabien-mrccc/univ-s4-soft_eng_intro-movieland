64fe3b2f31c385e213c31b5083f212678240b7f6
aminafanani <amina.fanani@etu.univ-amu.fr>
2024-02-05 17:32:53 +0100

    add void testToStringWithID()

--- app/src/test/java/moviesapp/model/MoviesTest.java
+++ app/src/test/java/moviesapp/model/MoviesTest.java
@@ -9,13 +9,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class MoviesTest {
     private final Movie movie1 = new Movie(true,null,null,"1",null,
-            null, null,0,null,null,null,true,
+            null, null,0,null,"2023",null,true,
             0, 0);
     private final Movie movie2 = new Movie(true,null,null,"2",null,
-            null, null,0,null,null,null,true,
+            null, null,0,null,"2023",null,true,
             0, 0);
     private final Movie movie3 = new Movie(true,null,null,"3",null,
-            null, null,0,null,null,null,true,
+            null, null,0,null,"2023",null,true,
             0, 0);
     Movies movies = new Movies();
     List<Movie> moviesToAddToMovies = new ArrayList<>();
@@ -28,5 +28,17 @@ public class MoviesTest {
         assertThat(moviesFull.toString().equals(
                 movie2 + "\n" + movie1 + "\n" + movie3 + "\n")).isTrue();
         assertThat(movies.toString().equals("Your list of movies is empty.")).isTrue();
+    }
+    @Test
+    void testToStringWithID(){
+        moviesToAddToMovies.add(movie2);
+        moviesToAddToMovies.add(movie1);
+        moviesToAddToMovies.add(movie3);
+        Movies moviesFull = new Movies(moviesToAddToMovies);
+        System.out.println(moviesFull.toStringWithID());
+        assertThat(moviesFull.toStringWithID().equals(
+                movie2.toStringWithID() + "\n" + movie1.toStringWithID() + "\n" +
+                        movie3.toStringWithID() + "\n")).isTrue();
+        assertThat(movies.toStringWithID().equals("Your list of movies is empty.")).isTrue();
     }
 }
