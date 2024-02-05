3b226dd77ea9c52d5f6e367d6fc2c93e55142d5b
numa <numa.efremon@etu.univ-amu.fr>
2024-02-05 09:27:28 +0100

    change id int to String

--- app/src/main/java/moviesapp/model/Movie.java
+++ app/src/main/java/moviesapp/model/Movie.java
@@ -2,7 +2,7 @@ package moviesapp.model;
 
 import java.util.List;
 
-public record Movie(boolean adult, String backdropPath, List<Integer> genreIds, int id, String originalLanguage,
+public record Movie(boolean adult, String backdropPath, List<Integer> genreIds, String id, String originalLanguage,
                     String originalTitle, String overview, double popularity, String posterPath, String releaseDate,
                     String title, boolean video, double voteAverage, int voteCount) {
     @Override
