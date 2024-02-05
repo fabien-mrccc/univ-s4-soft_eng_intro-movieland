005aa3f18b6a33591bd376ad394fca2bdf7bc2a1
numa <numa.efremon@etu.univ-amu.fr>
2024-02-05 11:53:22 +0100

    add public Iterator<Movie> iterator() add public Movie findMovie(String id , Movies movies) add public void remove(Movie movie)

--- app/src/main/java/moviesapp/model/Movies.java
+++ app/src/main/java/moviesapp/model/Movies.java
@@ -1,8 +1,9 @@
 package moviesapp.model;
 
+import java.util.Iterator;
 import java.util.List;
 
-public class Movies {
+public class Movies implements Iterable<Movie> {
     private final List<Movie> movies;
 
     public Movies(List<Movie> movies){
@@ -69,5 +70,51 @@ public class Movies {
      */
     public int size(){
         return movies.size();
+    }
+
+    @Override
+    public Iterator<Movie> iterator() {
+        return new MoviesIterator();
+    }
+
+    private class MoviesIterator implements Iterator<Movie> {
+        private int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index < size();
+        }
+
+        @Override
+        public Movie next() {
+            if (hasNext()) {
+                return movies.get(index++);
+            } else {
+                throw new IllegalStateException("No more elements in the iterator.");
+            }
+        }
+    }
+
+    /**
+     * Return of a movie selected with a list of movies and an id provided in parameter.
+     * @param id of movie to find
+     * @param movies to browse
+     * @return the movie found with selection or null
+     */
+    public Movie findMovie(String id , Movies movies){
+        for(Movie movie : movies){
+            if(movie.id().equals(id)){
+                return movie;
+            }
+        }
+        return null ;
+    }
+
+    /**
+     * Remove a movie from the movies list
+     * @param movie the movie that we want to remove in our list
+     */
+    public void remove(Movie movie){
+        movies.remove(movie);
     }
 }
