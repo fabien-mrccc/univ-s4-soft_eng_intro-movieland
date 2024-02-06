34d0845dd6d46691782d921683ad8cd025b1b17f
numa <numa.efremon@etu.univ-amu.fr>
2024-02-06 16:39:01 +0100

    fix public Movies findMovies(String name , String year) : add private methods according to jsonReaders methods

--- app/src/main/java/moviesapp/model/Favorites.java
+++ app/src/main/java/moviesapp/model/Favorites.java
@@ -85,15 +85,74 @@ public class Favorites {
     }
 
     /**
-     * take the list of favorites and return the list of movies of favorites
-     * @return a list of movies taken by the favorites
+     * Return a list of movies containing there information from the JSON file selected with a name(optional) and year (optional) provided in parameter.
+     * @param name the name of the movie
+     * @param year the release year of the movie
+     * @return return a list of movies
      */
-    public Movies findMovies(){
-        Movies movies = new Movies() ;
-        for(Movie movie : favorites){
-            movies.add(movie);
+    public Movies findMovies(String name , String year){
+        if(name == null || year == null){
+            return null;
         }
-        return movies ;
+
+        boolean nameEmpty = name.isEmpty();
+        boolean yearEmpty = year.isEmpty();
+
+        if(nameEmpty && yearEmpty){
+            return null;
+        }
+
+        Movies movieList = new Movies();
+
+        if(!nameEmpty && yearEmpty){
+            findMoviesByName(movieList , name);
+        }
+        else if(nameEmpty){
+            findMoviesByYear(movieList , year );
+        }
+        else{
+            findMovies(movieList , name , year);
+        }
+        return movieList;
+    }
+    /**
+     * Add to a list of movies the movie(s) from the JSON file selected with the name provided in parameter.
+     * @param movies is a list of movies to which we add the new movie(s) to the list
+     * @param name the name of the movie researched
+     */
+    private void findMoviesByName(Movies movies, String name ) {
+        for (Movie movie : favorites) {
+            if(movie.originalTitle().toLowerCase().contains(name.toLowerCase())) {
+                movies.add(movie);
+            }
+        }
+    }
+
+    /**
+     * Add to a list of movies the movie(s) from the JSON file selected with the year provided in parameter.
+     * @param movies is a list of movies to which we add the new movie(s) to the list
+     * @param year the year of the movie researched
+     */
+    private void findMoviesByYear(Movies movies , String year ) {
+        for (Movie movie : favorites) {
+            if(movie.releaseDate().toLowerCase().contains(year.toLowerCase())) {
+                movies.add(movie);
+            }
+        }
+    }
+    /**
+     * Add to a list of movies the movie(s) from the JSON file selected with the name and year provided in parameter.
+     * @param movies is a list of movies to which we add the new movie(s) to the list
+     * @param year the year of the movie researched
+     * @param name the name of the movie researched
+     */
+    private void findMovies(Movies movies, String name, String year) {
+        for (Movie movie : favorites) {
+            if(movie.releaseDate().toLowerCase().contains(year.toLowerCase())
+                    && movie.originalTitle().toLowerCase().contains(name.toLowerCase())) {
+                movies.add(movie);
+            }
+        }
     }
 
     /**
