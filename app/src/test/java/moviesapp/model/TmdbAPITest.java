d76738c97eb8fd0edfc815a66cb316aabe6177e7
fabien-mrccc <fabien.marcuccini@etu.univ-amu.fr>
2024-02-12 21:26:03 +0100

    rename TmdbAPITest.java

--- app/src/test/java/moviesapp/model/TmdbAPITest.java
+++ app/src/test/java/moviesapp/model/TmdbAPITest.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class TmdbApiTest {
+public class TmdbAPITest {
     private static TmdbAPI api;
     private static OkHttpClient client;
 
