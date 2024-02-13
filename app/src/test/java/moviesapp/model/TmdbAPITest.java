f51053ed0f2b17c06cd26c1f9b2e500bdaaab4f7
numa <numa.efremon@etu.univ-amu.fr>
2024-02-13 11:40:31 +0100

    fix void searchMovieTest() : modify test attribute

--- app/src/test/java/moviesapp/model/TmdbAPITest.java
+++ app/src/test/java/moviesapp/model/TmdbAPITest.java
@@ -19,7 +19,7 @@ public class TmdbAPITest {
 
     @Test
     void searchMovieTest() {
-        api.searchMovie("", "1890",null , null);
+        api.searchMovie("", "1890",null , null ,"1");
 
         String apiResultsJSONContent = readContentFromAPIResultsJSON();
         String dataExampleJSONContent = readContentFromDataExampleJSON();
