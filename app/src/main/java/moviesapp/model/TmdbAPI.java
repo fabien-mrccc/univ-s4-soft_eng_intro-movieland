78aff7b8b14a44c77dd8b3f62706d5c70d6340c2
aminafanani <amina.fanani@etu.univ-amu.fr>
2024-02-13 14:20:44 +0100

    order TmdbAPI.java elements

--- app/src/main/java/moviesapp/model/TmdbAPI.java
+++ app/src/main/java/moviesapp/model/TmdbAPI.java
@@ -1,5 +1,6 @@
 package moviesapp.model;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
@@ -16,51 +17,29 @@ import java.util.*;
 public class TmdbAPI {
 
     String fileName = System.getProperty("user.dir") + "/src/main/java/moviesapp/model/api-results.json";
-    String genreFileName = System.getProperty("user.dir") + "/src/main/java/moviesapp/model/genres.json";
+    private final static String genreFileName = System.getProperty("user.dir") + "/src/main/java/moviesapp/model/genres.json";
     OkHttpClient client = new OkHttpClient();
+    static OkHttpClient clientGenres = new OkHttpClient();
+
     private final static String baseUrl = "https://api.themoviedb.org/3";
     private final static String apiKey = "&api_key=5e40bf6f22600832c99dbb5d52115269";
     private final static String language = "language=en-US";
-    public static final TreeMap<String, String> GENRE_ID_MAP;
-
-    //liste of literally all genres available on TMDB
-    static {
-        GENRE_ID_MAP = new TreeMap<>();
-        GENRE_ID_MAP.put("action", "28");
-        GENRE_ID_MAP.put("adventure", "12");
-        GENRE_ID_MAP.put("animation", "16");
-        GENRE_ID_MAP.put("comedy", "35");
-        GENRE_ID_MAP.put("crime", "80");
-        GENRE_ID_MAP.put("documentary", "99");
-        GENRE_ID_MAP.put("drama", "18");
-        GENRE_ID_MAP.put("family", "10751");
-        GENRE_ID_MAP.put("fantasy", "14");
-        GENRE_ID_MAP.put("history", "36");
-        GENRE_ID_MAP.put("horror", "27");
-        GENRE_ID_MAP.put("music", "10402");
-        GENRE_ID_MAP.put("romance", "9648");
-        GENRE_ID_MAP.put("mystery", "10749");
-        GENRE_ID_MAP.put("science fiction", "878");
-        GENRE_ID_MAP.put("tv movie", "10770");
-        GENRE_ID_MAP.put("thriller", "53");
-        GENRE_ID_MAP.put("war", "10752");
-        GENRE_ID_MAP.put("western", "37");
-    }
+    public static final TreeMap<String, String> GENRE_ID_MAP = new TreeMap<>();
 
     /**
-     * return a list of every registered genres
+     * Return a list of every registered genres
      * @return list of every genre
      */
     public StringBuilder genreList(){
         StringBuilder list = new StringBuilder();
         for (String genre : GENRE_ID_MAP.keySet()) {
-            list.append("  -").append(genre).append("\n");
+            list.append("  •").append(genre).append("\n");
         }
         return list;
     }
 
     /**
-     * call every necessary methods to create the apropriate url from the given parameters
+     * Call every necessary methods to create the appropriate url from the given parameters
      * @param title part of or complete title of a movie
      * @param releaseYear year of release of movie
      * @param genres a list of genres
@@ -100,23 +79,6 @@ public class TmdbAPI {
             ObjectMapper mapper = JsonMapper.builder().build();
             ObjectNode node = mapper.readValue(result, ObjectNode.class);
             try (FileWriter fileWriter = new FileWriter(fileName, StandardCharsets.UTF_8)) {
-                mapper.enable(SerializationFeature.INDENT_OUTPUT);
-                mapper.writeValue(fileWriter, node);
-            }
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * turns the response of an api request into a json file filled with genres
-     * @param result response of the api request
-     */
-    private void requestToGenresFile(String result){
-        try {
-            ObjectMapper mapper = JsonMapper.builder().build();
-            ObjectNode node = mapper.readValue(result, ObjectNode.class);
-            try (FileWriter fileWriter = new FileWriter(genreFileName, StandardCharsets.UTF_8)) {
                 mapper.enable(SerializationFeature.INDENT_OUTPUT);
                 mapper.writeValue(fileWriter, node);
             }
@@ -182,13 +144,28 @@ public class TmdbAPI {
         return genreIds;
     }
 
-    public void updateGenresFile(){
+    /** Update the genres.json then fill the static GENRE_ID_MAP with the genres located in genres.json
+     * Update the genres.json then fill the static GENRE_ID_MAP with the genres located in genres.json
+     * @return the filled GENRE_ID_MAP
+     */
+    public static void fillGENRE_ID_MAP(){
+        updateGenresFile();
+        JSONReader jsonGenresReader = new JSONReader(genreFileName);
+        for(JsonNode genre : jsonGenresReader.getJsonGenres()){
+            GENRE_ID_MAP.put(genre.get("name").asText(),genre.get("id").asText());
+        }
+    }
+
+    /**
+     * Update the genres.json with all genres from Tmdb
+     */
+    private static void updateGenresFile(){
         String url = baseUrl + "/genre/movie/list?language=en" + apiKey;
 
         Request request = new Request.Builder().url(url).build();
 
         try {
-            Response response = client.newCall(request).execute();
+            Response response = clientGenres.newCall(request).execute();
 
             if(response.isSuccessful()){
                 String genresResult = response.body().string();
@@ -198,6 +175,23 @@ public class TmdbAPI {
                 System.out.println("error :" + response.code());
             }
         }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Turns the response of an api request into a json file filled with genres
+     * @param result response of the api request
+     */
+    private static void requestToGenresFile(String result){
+        try {
+            ObjectMapper mapper = JsonMapper.builder().build();
+            ObjectNode node = mapper.readValue(result, ObjectNode.class);
+            try (FileWriter fileWriter = new FileWriter(genreFileName, StandardCharsets.UTF_8)) {
+                mapper.enable(SerializationFeature.INDENT_OUTPUT);
+                mapper.writeValue(fileWriter, node);
+            }
+        }catch (IOException e){
             e.printStackTrace();
         }
     }
