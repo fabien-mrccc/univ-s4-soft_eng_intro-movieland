4381764de917c3331e8fef878a587b536e500905
numa <numa.efremon@etu.univ-amu.fr>
2024-02-15 00:59:04 +0100

    fix private void searchMoviesToPrint() : check if searchmovie is true or false
    private Boolean searchMovies() : check if the user put information

--- app/src/main/java/moviesapp/controller/CLController.java
+++ app/src/main/java/moviesapp/controller/CLController.java
@@ -86,8 +86,12 @@ public final class CLController {
      * Ask title, release year, vote average and genres information to the user to select a specific group of movies to print
      */
     private void searchMoviesToPrint(){
-        searchMovies();
-        System.out.println("\nYour list of movies found in your search: \n" + jsonReader.findAllMovies());
+        if(searchMovies()){
+            System.out.println("\nYour list of movies found in your search: \n" + jsonReader.findAllMovies());
+        }
+        else{
+            System.out.println("Please put information: ");
+        }
     }
 
     /**
@@ -102,20 +106,31 @@ public final class CLController {
     /**
      * Ask title, release year, vote average and genres information to the user to select a specific group of movies
      */
-    private void searchMovies(){
-        do{
-            TmdbAPI api = new TmdbAPI();
-            String title = askValue("Title of the movie: ");
-            String releaseYear = askValue("Year of release: ");
-            String voteAverage = askValue("Movie's minimum rate: ");
-            List<String> genres = specifiedGenres(api);
-            api.searchMovie(title, releaseYear, genres, voteAverage , "1");
-            jsonReaderUpdate();
-            String page = askValue("Select your page [total pages = " + jsonReader.numberOfPagesOfMoviesInJson() + "]: ");
-            api.searchMovie(title, releaseYear, genres, voteAverage , page);
-            jsonReaderUpdate();
-            System.out.println("\nYour list of movies found in your search: \n" + jsonReader.findAllMovies());
-        } while(askToConfirm("Do you want to watch another page? : "));
+    private Boolean searchMovies(){
+        TmdbAPI api = new TmdbAPI();
+        boolean acceptation = true ;
+        String title = askValue("Title of the movie: ");
+        String releaseYear = askValue("Year of release: ");
+        String voteAverage = askValue("Movie's minimum rate: ");
+        List<String> genres = specifiedGenres(api);
+
+        if(title.isEmpty() && releaseYear.isEmpty() && voteAverage.isEmpty() && genres.isEmpty()){
+            System.out.println("no information sent ");
+            acceptation = false;
+            return acceptation;
+        }
+
+        else{
+            do{
+                api.searchMovie(title, releaseYear, genres, voteAverage , "1");
+                jsonReaderUpdate();
+                String page = askValue("Select your page [total pages = " + jsonReader.numberOfPagesOfMoviesInJson() + "]: ");
+                api.searchMovie(title, releaseYear, genres, voteAverage , page);
+                jsonReaderUpdate();
+                System.out.println("\nYour list of movies found in your search: \n" + jsonReader.findAllMovies());
+            } while(askToConfirm("Do you want to watch another page? : "));
+            return acceptation;
+        }
     }
 
     /**
