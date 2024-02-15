946f07435fbf04b79e7e7990039f668fdd400b0b
MARCUCCINI Fabien <fabien.marcuccini@etu.univ-amu.fr>
2024-02-15 17:55:52 +0100

    remove help command message to user

--- app/src/main/java/moviesapp/AppCLI.java
+++ app/src/main/java/moviesapp/AppCLI.java
@@ -8,7 +8,6 @@ public class AppCLI {
     public static void main(String[] args) {
         fillGENRE_ID_MAP();
         System.out.println("\nWelcome to the movies app!");
-        System.out.println("Type 'help' to learn application commands.");
         CLController controller = new CLController();
         controller.select();
     }
