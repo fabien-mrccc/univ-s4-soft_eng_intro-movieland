72dba85a7546275c8cdc7304d9c61ebff1f7e98c
aminafanani <amina.fanani@etu.univ-amu.fr>
2024-02-16 14:48:05 +0100

    add public void activateButton(Button button) add public static Button getGoButton()

--- app/src/main/java/moviesapp/viewer/OptionButton.java
+++ app/src/main/java/moviesapp/viewer/OptionButton.java
@@ -1,4 +1,21 @@
 package moviesapp.viewer;
+import javafx.scene.control.Button;
 
 public class OptionButton {
+    private static final Button goButton = new Button("Go");
+
+    public void activateButton(Button button){
+        if(button.isPressed()){
+            switch(button.getText()){
+                case "Go":
+                    //TODO: call the methode that print all the movies resulting from the user research;
+                    break;
+
+            }
+        }
+    }
+
+    public static Button getGoButton(){
+        return goButton;
+    }
 }
