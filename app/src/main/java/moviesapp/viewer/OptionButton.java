fe3a8a6ddeac45ccb16ffed661879e539d52807f
aminafanani <amina.fanani@etu.univ-amu.fr>
2024-02-19 14:24:56 +0100

    fix public void activateButton(Button button) add public static Button getRemoveFromFavoritesButton()

--- app/src/main/java/moviesapp/viewer/OptionButton.java
+++ app/src/main/java/moviesapp/viewer/OptionButton.java
@@ -5,6 +5,7 @@ public class OptionButton {
     private static final Button goButton = new Button("Go");
     private static final Button favoritesButton = new Button("Favorites");
     private static final Button addToFavoritesButton = new Button("add");
+    private static final Button removeFromFavoritesButton = new Button("remove");
     public void activateButton(Button button){
         if(button.isPressed()){
             switch(button.getText()){
@@ -19,6 +20,10 @@ public class OptionButton {
                 case "add":
                     //TODO: call the methode that adds a film the favorites;
                     break;
+
+                case "remove":
+                    //TODO: call the methode that remove a film from favorites;
+                    break;
             }
         }
     }
@@ -31,5 +36,8 @@ public class OptionButton {
     }
     public static Button getAddToFavoritesButton(){
         return addToFavoritesButton;
+    }
+    public static Button getRemoveFromFavoritesButton(){
+        return removeFromFavoritesButton;
     }
 }
