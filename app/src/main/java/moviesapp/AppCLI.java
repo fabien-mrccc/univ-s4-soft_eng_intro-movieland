06e3e8e4703f849ff91e3b6f0147957e7f239da7
MARCUCCINI Fabien <fabien.marcuccini@etu.univ-amu.fr>
2024-02-19 17:37:44 +0100

    update class with new fillGENRE_NAME_ID_MAP() name

--- app/src/main/java/moviesapp/AppCLI.java
+++ app/src/main/java/moviesapp/AppCLI.java
@@ -6,7 +6,7 @@ import static moviesapp.model.TmdbAPI.*;
 
 public class AppCLI {
     public static void main(String[] args) {
-        fillGENRE_ID_MAP();
+        fillGENRE_NAME_ID_MAP();
         System.out.println("\nWelcome to the movies app!");
         CLController controller = new CLController();
         controller.select();
