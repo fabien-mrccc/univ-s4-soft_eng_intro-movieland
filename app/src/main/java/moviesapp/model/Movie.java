d0f21ff2296b79ebd63604fb2c66404082296412
MARCUCCINI Fabien <fabien.marcuccini@etu.univ-amu.fr>
2024-02-19 17:33:38 +0100

    List<Integer> genreIds to List<String> genres attribute

--- app/src/main/java/moviesapp/model/Movie.java
+++ app/src/main/java/moviesapp/model/Movie.java
@@ -2,7 +2,7 @@ package moviesapp.model;
 
 import java.util.List;
 
-public record Movie(boolean adult, String backdropPath, List<Integer> genreIds, String id, String originalLanguage,
+public record Movie(boolean adult, String backdropPath, List<String> genres, String id, String originalLanguage,
                     String originalTitle, String overview, double popularity, String posterPath, String releaseDate,
                     String title, boolean video, double voteAverage, int voteCount) {
     @Override
@@ -32,7 +32,7 @@ public record Movie(boolean adult, String backdropPath, List<Integer> genreIds, 
     public String details(){
         return "__________________________  \n"+"movie n°"+ id +":\n  title: "+title+"\n  original title: "+originalTitle+
                 "\n  release date: "+releaseDate+ "\n  original language: "+originalLanguage+
-                "\n  genreIds: "+genreIds+"\n  popularity: " +popularity+"\n  adult: "+adult+
+                "\n  genreIds: "+genres+"\n  popularity: " +popularity+"\n  adult: "+adult+
                 "\n  vote average: "+voteAverage+"\n  vote count: " +voteCount+"\n  overview: "
                 +overview+ "\n__________________________  ";
     }
