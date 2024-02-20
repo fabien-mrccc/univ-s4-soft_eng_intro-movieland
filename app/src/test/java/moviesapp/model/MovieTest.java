76911b60823c9d9862600c2061e32945fe099553
MARCUCCINI Fabien <fabien.marcuccini@etu.univ-amu.fr>
2024-02-20 12:15:58 +0100

    add static void setup() update all tests

--- app/src/test/java/moviesapp/model/MovieTest.java
+++ app/src/test/java/moviesapp/model/MovieTest.java
@@ -1,35 +1,43 @@
 package moviesapp.model;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.List;
 public class MovieTest {
+
+    static List<String> genres;
+
+    @BeforeAll
+    static void setup(){
+        genres = new ArrayList<>();
+        genres.add("action");
+    }
+
     @Test
     void testDetails(){
-        List<Integer> genreIds = new ArrayList<>();
-        genreIds.add(0);
-        Movie movie1 = new Movie(true,"backdropPath",genreIds,"1","originalLanguage",
+        Movie movie1 = new Movie(true,"backdropPath",genres,"1","originalLanguage",
                 "originalTitle","overview",2,"posterPath","releaseDate",
                 "title",false,3,4);
         assertThat(movie1.details()).isEqualTo("__________________________  \nmovie n°1:\n  title: title\n  original title: originalTitle"+
-                "\n  release date: releaseDate\n  original language: originalLanguage\n  genreIds: "+genreIds+
+                "\n  release date: releaseDate\n  original language: originalLanguage\n  genreIds: "+genres+
                 "\n  popularity: 2.0\n  adult: true\n  vote average: 3.0\n  vote count: 4\n  overview: overview\n__________________________  ");
     }
 
     @Test
     void testToString(){
-        List<Integer> genreIds = new ArrayList<>();
-        genreIds.add(0);
-        Movie movie1 = new Movie(true,"backdropPath",genreIds,"1","originalLanguage",
+        Movie movie1 = new Movie(true,"backdropPath",genres,"1","originalLanguage",
                 "originalTitle","overview",2,"posterPath","releaseDateTest",
                 "title",false,3,4);
 
-        String expectedString =   " |__________________" +
-                "\n  title: title" +
-                "\n  vote average: 3.0" +
-                "\n  release year: rele\n" +
-                "__________________________\n";
+        String expectedString = """
+                 |__________________
+                  title: title
+                  vote average: 3.0
+                  release year: rele
+                __________________________
+                """;
 
         assertThat(movie1.toString()).isEqualTo(expectedString);
 
@@ -37,9 +45,7 @@ public class MovieTest {
 
     @Test
     void testToStringWithID(){
-        List<Integer> genreIds = new ArrayList<>();
-        genreIds.add(0);
-        Movie movie1 = new Movie(true,"backdropPath",genreIds,"1","originalLanguage",
+        Movie movie1 = new Movie(true,"backdropPath",genres,"1","originalLanguage",
                 "originalTitle","overview",2,"posterPath","releaseDate",
                 "title",false,3,4);
         assertThat(movie1.toStringWithID())
