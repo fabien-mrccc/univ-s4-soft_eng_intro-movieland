049f68abe9e4ac070a474a7625c299bde1074e26
MARCUCCINI Fabien <fabien.marcuccini@etu.univ-amu.fr>
2024-02-27 00:22:29 +0100

    clean code after merge

--- app/src/main/java/moviesapp/controller/GUI/AppController.java
+++ app/src/main/java/moviesapp/controller/GUI/AppController.java
@@ -1,43 +1,60 @@
 package moviesapp.controller.GUI;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import moviesapp.model.api.Genres;
+import moviesapp.model.json.JsonReader;
 import moviesapp.model.movies.Movie;
-import moviesapp.viewer.buttons.*;
 import moviesapp.viewer.left_panel.LeftPanelView;
 import moviesapp.viewer.left_panel.WithTitlePanelView;
 import moviesapp.viewer.left_panel.WithoutTitlePanelView;
+import moviesapp.viewer.right_panel.DetailsMode;
 import moviesapp.viewer.right_panel.ImagePanelView;
 import moviesapp.viewer.right_panel.RightPanelView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static moviesapp.model.json.JsonReader.apiFilePath;
+
 public class AppController implements Initializable {
+
+    private WithTitlePanelView withTitlePanelViewComponent;
+    private WithoutTitlePanelView withoutTitlePanelViewComponent;
+    private ImagePanelView imagePanelViewComponent;
 
     @Override
     public void initialize(URL location, ResourceBundle resourceBundle) {
 
+        genreSelectorSetUp();
         turnOnSearchWithTitleMode();
         setGUIComponents();
     }
 
+    private void genreSelectorSetUp(){
+        Genres.fillGENRE_NAME_ID_MAP();
+        ObservableList<String> genres = FXCollections.observableArrayList(Genres.instance.genreList());
+        genreListView.setItems(genres);
+        genreListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+    }
+
     private void setGUIComponents(){
-        leftPanelViewGUI = new LeftPanelView(mainAnchorPane, leftPane, appTitle, selectModePane, withTitleButton, withoutTitleButton);
+        new LeftPanelView(mainAnchorPane, leftPane, appTitle, selectModePane, withTitleButton, withoutTitleButton);
 
-        withTitlePanelViewGUI = new WithTitlePanelView(leftPane, appTitle, titleAndSearchPane, title, searchBar, yearPane, yearLabel, yearField,
-                favoritesWithTitlePane, favoritesWithTitleButton, goWithTitlePane, goWithTitleButton );
+        withTitlePanelViewComponent = new WithTitlePanelView(leftPane, appTitle, titleAndSearchPane, title, searchBar, yearPane, yearLabel, yearField,
+                favoritesWithTitlePane, favoritesWithTitleButton, goWithTitlePane, goWithTitleButton);
 
-        withoutTitlePanelViewGUI = new WithoutTitlePanelView(leftPane, appTitle, yearsPane, years, from, singleOrMinYearField,
-                to, maxYearField, genresPane, genres, ratingPane, rating, atLeast, ratingField, searchBar, genreListView);
+        withoutTitlePanelViewComponent = new WithoutTitlePanelView(leftPane, appTitle, yearsPane, years, from, singleOrMinYearField,
+                to, maxYearField, genresPane, genres, ratingPane, rating, atLeast, ratingField, searchBar, genreListView, buttonsWithoutTitlePane,
+                favoritesWithoutTitleButton, goWithoutTitleButton);
 
-        withoutTitleButtonsGUI = new WithoutTitleButtons(buttonsWithoutTitlePane, leftPane, ratingPane, favoritesWithoutTitleButton, goWithoutTitleButton);
+        new RightPanelView(leftPane, mainAnchorPane, rightStackPane, rightScrollPane);
 
-        rightPanelViewGUI = new RightPanelView(leftPane, mainAnchorPane, rightStackPane, rightScrollPane);
-
-        imagePanelViewGUI = new ImagePanelView(gridPane, rightScrollPane);
+        imagePanelViewComponent = new ImagePanelView(gridPane, rightScrollPane);
     }
 
     @FXML
@@ -57,27 +74,32 @@ public class AppController implements Initializable {
     }
 
     @FXML
-    public void favoritesButtonClicked(){
-        //imagePanelView.distributeImages(Favorites.instance.getFavorites());
+    private void searchCatcherNoTitle(){
+        withoutTitlePanelViewComponent.searchCatcherNoTitle();
+        updateImagePanelView();
+    }
+
+    @FXML
+    private void searchCatcherWithTitle(){
+        withTitlePanelViewComponent.searchCatcherWithTitle();
+        updateImagePanelView();
+    }
+
+    private void updateImagePanelView(){
+        JsonReader jsonReader = new JsonReader(apiFilePath);
+        imagePanelViewComponent.distributeImages(jsonReader.findAllMovies());
     }
 
     public static void handleClickOnImage(Movie movie) {
-        new DetailsButton(movie);
+        new DetailsMode(movie);
     }
 
 
-    /////////////////////////////////////////////////////////// Begin GUI Components
 
-    private LeftPanelView leftPanelViewGUI;
-    private WithTitlePanelView withTitlePanelViewGUI;
-    private FavoritesWithTitleButton favoritesWithTitleButtonGUI;
-    private GoWithTitleButton goWithTitleButtonGUI;
-    private WithoutTitlePanelView withoutTitlePanelViewGUI;
-    private WithoutTitleButtons withoutTitleButtonsGUI;
-    private RightPanelView rightPanelViewGUI;
-    private ImagePanelView imagePanelViewGUI;
 
-    /////////////////////////////////////////////////////////// End GUI Components
+
+
+
 
 
     /////////////////////////////////////////////////////////// Begin FXML Identifiers
